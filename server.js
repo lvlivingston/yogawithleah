8520615d19e8a6c321aa1a759d8156c0c62233fb
lvlivingston <lvlivingston@gmail.com>
2024-01-30 14:14:49 -0600

    updated small link favicon

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Configure both serve-favicon & static middleware
 // to serve from the production 'build' folder
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'build', 'lotus-black.png')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 const port = process.env.PORT || 3001;
