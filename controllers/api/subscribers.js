d64a93d68f58434f299313d958da823348f889cf
lvlivingston <lvlivingston@gmail.com>
2024-01-31 12:05:54 -0600

    fixed controller

--- controllers/api/subscribers.js
+++ controllers/api/subscribers.js
@@ -6,10 +6,15 @@ module.exports = {
   
 async function create(req, res) {
   try {
-    const subscriber = await Subscriber.create(req.body);
-    console.log("create subscriber");
-  } catch (err) {
-    res.status(400).json(err);
-    console.log(err)
+      const { email } = req.body;
+      const existingSubscriber = await Subscriber.findOne({ email });
+      if (existingSubscriber) {
+          return res.status(409).json({ error: 'Your email is already on the list.' });
+      }
+      const subscriber = await Subscriber.create({ email });
+      res.status(201).json(subscriber);
+  } catch (error) {
+      console.error('Error creating subscriber:', error);
+      res.status(500).json({ error: 'Internal server error' });
   }
 }
